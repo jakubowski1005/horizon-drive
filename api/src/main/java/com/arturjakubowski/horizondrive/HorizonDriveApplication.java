ee42aff7673bd068fbf3bd6d7802837de31d481f
Artur Jakubowski <jakubowski1005@gmail.com>
2021-04-24 18:11:14 +0200

    Init api

---
+++ api/src/main/java/com/arturjakubowski/horizondrive/HorizonDriveApplication.java
@@ -0,0 +1,13 @@
+package com.arturjakubowski.horizondrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonDriveApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/arturjakubowski/horizondrive/HorizonDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturjakubowski.horizondrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
